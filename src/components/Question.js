87ad476df23a8368a6a2b8725502c2f3bc6a6f0f
Edsey omenda <omendajames0317@gmail.com>
2022-07-17 22:25:33 +0300

    completed lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  const [count, setCount] = useState(0);
 
   // add useEffect code
+  useEffect(()=>{
+
+    if(timeRemaining === 0){
+      return onAnswered(false);
+    }
+
+    const timerId = setTimeout(() =>{ setTimeRemaining(count>1? count -1: 0)
+      // console.log("time decreases by one second")
+    },1000);
+    setCount(timeRemaining)
+
+    return function cleanup(){
+      clearTimeout(timerId)
+    }
+
+
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -11,6 +29,7 @@ function Question({ question, onAnswered }) {
   }
 
   const { id, prompt, answers, correctIndex } = question;
+ 
 
   return (
     <>
